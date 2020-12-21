1c2846c5f072484dc0a0064e041f6735984053e6
AndrGen <31141640+AndrGen@users.noreply.github.com>
2020-12-21 16:30:36 +0300

    замена фразы на английском, фразой на русском

---
+++ PostgreSQL/translate_answer().sql
@@ -0,0 +1,23 @@
+﻿CREATE OR REPLACE FUNCTION public.translate_ans(IN _ans TEXT)
+  RETURNS TEXT AS
+  $BODY$     
+    BEGIN 
+
+    RETURN (
+	     coalesce(
+			(
+			    select string_agg(rus_text, ', ') 
+			    from question_ans_locale 
+			    where tag in (
+						select regexp_split_to_table(_ans, ';')  
+				         )
+			), _ans
+	             )
+	   ); 
+    
+    END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
+
