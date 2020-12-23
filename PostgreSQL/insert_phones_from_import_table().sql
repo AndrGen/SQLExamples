d484b43e832c79cdd0039506fe103711eb745f5f
AndrGen <31141640+AndrGen@users.noreply.github.com>
2020-12-23 15:00:24 +0300

    импорт из временной таблицы, статистика по ответам

---
+++ PostgreSQL/insert_phones_from_import_table().sql
@@ -0,0 +1,110 @@
+﻿DROP FUNCTION public.phones_from_temp_table(bigint, text, integer);
+
+CREATE OR REPLACE FUNCTION public.phones_from_temp_table(dj_id bigint, descr text, countload integer)
+  RETURNS SETOF text AS
+$BODY$ 
+    DECLARE
+       _cnt_imported bigint := 0;  
+       _cnt_inserted_dj_record bigint := 0; 
+       _last_insert_id bigint :=0;
+       _dj_load_id bigint :=0;
+    BEGIN
+
+      if (
+           select true
+           from project p
+           inner join dj dj on dj.project_id = p.id
+           where p.shortname not ilike '%test%' and dj.id = $1
+         )    
+      then  
+         delete from import_phones
+	 where phone in( 
+		select ip.phone 
+		from import_phones ip
+		inner join dj_record dr on dr.phone = ip.phone
+		where dr.dj_id = $1
+		);
+	 return next cast('Duplicates deleted'  as text);	 
+      else 
+         return next cast('Test project'  as text);	  	
+      end if;
+     
+    
+      SELECT count(*) from import_phones INTO _cnt_imported;
+      IF _cnt_imported = 0 THEN RAISE EXCEPTION 'Imported records not found';
+      END IF;   
+      return next cast('Count records for import ' || _cnt_imported as text);   
+
+
+      EXECUTE 'CREATE TEMP TABLE temp_dj_record_phone AS 
+                            (
+				with i as (INSERT INTO dj_record_' || EXTRACT(month FROM now()) || '_' ||  EXTRACT(year FROM now()) || '(dj_id, phone, in_queue)
+				                       (select ' || dj_id || ', phone, ''t'' from import_phones) returning id, phone) 
+				select id,  phone
+				from i 
+                           )'; 
+     
+      SELECT count(*) from temp_dj_record_phone INTO _cnt_inserted_dj_record;
+      IF _cnt_inserted_dj_record = 0 THEN RAISE EXCEPTION 'In dj_record nothing insert';
+      END IF;   
+      return next cast('Inserted into dj_record  ' || _cnt_inserted_dj_record as text);  
+
+
+      INSERT INTO  dj_load(dj_id, overview, countload) VALUES(dj_id, descr, countload) RETURNING id INTO  _dj_load_id;
+      IF _dj_load_id = 0 THEN RAISE EXCEPTION 'dj load not inserted';
+      END IF;   
+      return next cast('_dj_load_id = ' || _dj_load_id as text) ;
+      
+      
+      EXECUTE 'INSERT INTO dj_record_ext_' || EXTRACT(month FROM now()) || '_' ||  EXTRACT(year FROM now()) ||
+		    '(dj_record_id, cause_codes_id, callafterdate, load_id, timezone_id, dop1, dop2, dop3, dop4, dop5)
+		     ( SELECT 
+			id, 
+			(select id from cause_codes where code_name  = ''QUEUE''),
+			now(),
+			' || _dj_load_id  || ',
+			(select coalesce( (select id from time_zone where utc_offset = (select coalesce(timezone::text , ''3''::text)) OR code = (select coalesce(timezone::text , ''MSK''::text))), (select id from time_zone where code = ''MSK''))), 
+			dop1, 
+			dop2,
+			dop3, 
+			dop4, 
+			dop5
+		     from temp_dj_record_phone tdrp 
+		     inner join import_phones tip on tip.phone = tdrp.phone
+		  );' ;   
+		  
+      update dj_record_ext dre
+      set cause_codes_id = (select id from cause_codes where code_name  = 'BLACK_LIST') 
+      FROM dj_record AS dr
+      where
+           dr.id = dre.dj_record_id and
+           phone in( 
+		select ip.phone 
+		from import_phones ip
+		inner join black_list bl on bl.phone = ip.phone
+		);
+      return next cast('Black list scanned'  as text); 	
+
+      update dj_record_ext dre
+      set cause_codes_id = (select id from cause_codes where code_name  = 'BLACK_LIST') 
+      FROM dj_record AS dr
+      where 
+             dr.id = dre.dj_record_id and
+             phone in( 
+		select ip.phone 
+		from import_phones ip
+		where (select regexp_matches(ip.phone,  phone_template) from black_list_template ) is not null
+		);
+      return next cast('Black list with templates scanned'  as text); 	
+
+
+
+      drop table temp_dj_record_phone;
+      
+     return next 'Import ended';
+    END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
+
---
+++ PostgreSQL/question_answers_part_stat().sql
@@ -0,0 +1,113 @@
+﻿CREATE OR REPLACE FUNCTION public.q_answers_stat(
+    IN _dj_id bigint,
+    IN _q_number_show integer,
+    IN _condition text,
+    IN _q_numbers_filter text,
+    IN _q_answers_filter text)
+  RETURNS TABLE(id bigint, dj_id bigint, descr text, q_number integer, answers_count bigint, answer_name text) AS
+$BODY$ 
+DECLARE
+   _select_str text := '
+			select 
+				min(q.id) as id,
+				dj.id as dj_id,
+				q.descr,  
+				max(q.sort_order) as q_number,
+				count(qa.answer) as answers_count,
+				(select coalesce(rus_text, qa.answer) from question_answer_locale where tag = qa.answer) as answer_name
+			from question q 
+			inner join app_question_staff aqs on aqs.question_id = q.id 
+			inner join app a on a.id = aqs.app_id and not a.archive 
+			inner join dialjob dj on a.id = dj.app_id and not dj.archive 
+			inner join dialjob_record dr on dr.dj_id = dj.id 
+			inner join dialjob_record_ext dre on dre.dialjob_record_id = dr.id and not dre.archive and dre.id in (
+			';
+ 			 
+   _where_str text := '
+			 )
+			left join question_answer qa on dre.id = qa.dialjob_record_ext_id and q.id = qa.question_id  
+			where not q.archive
+                       ';
+  _select_sub_query_str text := '
+				(select dre.id
+				from question_answer qa
+				inner join question q on qa.question_id = q.id and not q.archive
+				inner join dialjob_record_ext dre on dre.id = qa.dialjob_record_ext_id and not dre.archive 
+				inner join dialjob_record dr on dre.dialjob_record_id = dr.id
+                                ';  
+  _group_order_sub_query_str text := '
+				group by dre.id
+				order by dre.id)
+				';   
+
+  _groupby_str text := '
+			group by q.descr, dj.id, qa.answer
+			order by q_number, id;
+			'; 
+   
+  _total_query text;	
+
+  _cnt_filter integer := 0;	
+
+  _i int := 0;	
+
+  _sub_query text;	                
+BEGIN
+
+	RAISE NOTICE 'input parameters: % | % | % | % | %', _dj_id ,_q_number_show, _condition, _q_numbers_filter, _q_answers_filter;
+
+	if _dj_id is null or _q_number_show is null or _condition is null or trim(_condition) = '' or _q_numbers_filter is null or trim(_q_numbers_filter) = '' or _q_answers_filter is null or trim(_q_answers_filter) = '' then 
+				RAISE Exception 'One of input vars not found'; 
+	end if;
+
+        select count(*) from regexp_split_to_table(_q_numbers_filter, ';') into _cnt_filter; 
+
+        LOOP	
+		--RAISE NOTICE 'regexp_split_to_table %', (select * from regexp_split_to_table(_q_filter, ';') limit 1 offset _i);
+                SELECT concat(
+				_select_sub_query_str, 
+				' where dj_id = ',
+				_dj_id, 
+				' and sort_order = ',
+				(select * from regexp_split_to_table(_q_numbers_filter, ';') limit 1 offset _i),
+				' and answer = ''',
+                                (select coalesce(tag, '') from question_answer_locale where rus_text = 
+										(select * from regexp_split_to_table(_q_answers_filter, ';') limit 1 offset _i)),
+				'''',
+				_group_order_sub_query_str,
+				_condition,
+				_sub_query
+			     ) into _sub_query;
+
+		_i := _i + 1; 
+		EXIT WHEN (_i = _cnt_filter);
+        
+        END LOOP;
+
+        _sub_query := left(_sub_query, length(_sub_query) - length(_condition));
+
+	--RAISE NOTICE 'Sub query: %', _sub_query;
+
+        SELECT concat(
+			     _select_str,
+			     _sub_query,
+			     _where_str,
+			     ' and dj_id = ',
+			     _dj_id,
+			     ' and sort_order = ',
+			     _q_number_show,
+			     _groupby_str
+                     ) into _total_query;
+
+        RAISE NOTICE 'Total query: %', _total_query;
+
+        RETURN QUERY
+        EXECUTE (_total_query); 
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
+
+
+-- select * from q_answers_stat(326,3,'intersect all','1;2','Первый вариант;Нет');
